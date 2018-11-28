b8ecdb722f4aa6c4cd75ac665d37b2281118f96e
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-11-28 12:11:07 -0800

    adds 404 and default cra image

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Router, Route, Redirect, Switch } from "react-router-dom";
 import { history } from "./utils";
-import { CarsListing, Car } from "./containers";
+import { CarsListing, Car } from "./pages";
 
 const App = () => (
   <Router history={history}>
     <Switch>
       <Route exact path="/" component={CarsListing} />
-      <Route path="/car/:id" component={Car} />
+      <Route exact path="/car/:id" component={Car} />
 
       <Redirect from="*" to="/" />
     </Switch>
