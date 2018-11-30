55ead1a3c6f55829d969013e84031918defd87f1
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-11-29 16:17:43 -0800

    style car page

--- src/components/CarDetails/index.jsx
+++ src/components/CarDetails/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { moneyConvert } from "../../utils";
+import styles from "./styles.sass";
 
 // this is approximate values
 const MONTHLY_RATE = 0.45; // 45% from monthly payment
 const START_RATE = 0.6; // 60% from start fee
 
 const CarDetails = ({ vehicle = {} }) => {
-  const { make, model, meleage, modelYear, productFinancials = [] } = vehicle;
+  const { make, model, mileage, modelYear, productFinancials = [] } = vehicle;
 
   const financials = productFinancials[0] ? productFinancials[0] : [];
   const monthlyPayment = moneyConvert(financials.monthly_payment_cents);
@@ -18,21 +19,37 @@ const CarDetails = ({ vehicle = {} }) => {
   const startPaymentLoan = moneyConvert(financials.start_fee_cents, START_RATE);
 
   return (
-    <section>
-      <p>
-        {modelYear} {make}
+    <section className={styles.details}>
+      <div>
+        <p className={styles.title}>
+          {modelYear} {make}
+        </p>
+        <p className={styles.model}>{model}</p>
+        <p className={styles.mileage}>{mileage} Mi.</p>
+      </div>
+      <div className={styles.paymentMontly}>
+        <p>
+          Monthly pymt.
+          <span className={styles.orange}>{monthlyPayment}</span>
+        </p>
+        <p>
+          Start pymt.
+          <span>{startFee}</span>
+        </p>
+      </div>
+      <p className={styles.lineAfter}>
+        <span>3Yr Loan Alternative</span>
       </p>
-      <p>{model}</p>
-      <p>{meleage}</p>
-      <p>Monthly pymt.</p>
-      <p>{monthlyPayment}</p>
-      <p>Start pymt.</p>
-      <p>{startFee}</p>
-      <p>3Yr Loan Alternative</p>
-      <p>Monthly pymt.</p>
-      <p>{monthlyPaymentLoan}</p>
-      <p>Start pymt.</p>
-      <p>{startPaymentLoan}</p>
+      <div className={styles.paymentLoan}>
+        <p>
+          Monthly pymt.
+          <span> {monthlyPaymentLoan}</span>{" "}
+        </p>
+        <p>
+          Start pymt.
+          <span> {startPaymentLoan} </span>
+        </p>
+      </div>
     </section>
   );
 };
